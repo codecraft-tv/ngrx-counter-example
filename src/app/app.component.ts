1daac7ffb6f8d6fd8217b96842a86bf6f4825216
Asim Hussain <jawache@gmail.com>
2017-01-25 23:03:36 +0000

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {Store} from "@ngrx/store";
+import {Observable} from "rxjs/Observable";
+import {INCREMENT, DECREMENT, RESET} from './app.reducers';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'counter',
+  template: `
+    <div class="content">
+        <button (click)="increment()">+</button>
+        <button (click)="decrement()">-</button>
+        <button (click)="reset()">Reset</button>
+        <h3>{{counter$ | async}}</h3>
+    </div>
+    `
 })
-export class AppComponent {
-  title = 'app works!';
+export class CounterComponent {
+  counter$: Observable<number>;
+
+  constructor(private store: Store<number>) {
+    this.counter$ = this.store.select<number>('counter')
+  }
+
+  increment() {
+    this.store.dispatch({type: 'INCREMENT'});
+  }
+
+  decrement() {
+    this.store.dispatch({type: 'DECREMENT'});
+  }
+
+  reset() {
+    this.store.dispatch({type: 'RESET'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {StoreModule} from '@ngrx/store';
+import {CounterComponent} from './app.component';
+import {counterReducer} from './app.reducers';
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 
 @NgModule({
   declarations: [
-    AppComponent
+    CounterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    StoreModule.provideStore({counter: counterReducer}),
+
+    StoreDevtoolsModule.instrumentOnlyWithExtension({
+      maxAge: 10
+    })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CounterComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
